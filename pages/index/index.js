5bf8cd4616da541bfac364e46fd315bcc6d87bc6
lynn <lynn6186127@gmail.com>
2019-05-12 20:46:27 +1200

    navigation

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,47 +13,36 @@ Page({
       "军事",
       "体育",
       "其他"
+    ],
+  },
 
-    ],
-    objectArray: [
-      { id: 5, unique: 'unique_5' },
-      { id: 4, unique: 'unique_4' },
-      { id: 3, unique: 'unique_3' },
-      { id: 2, unique: 'unique_2' },
-      { id: 1, unique: 'unique_1' },
-      { id: 0, unique: 'unique_0' },
-    ],
-    numberArray: [1, 2, 3, 4],
-  },
-  switch(e) {
-    const length = this.data.objectArray.length
-    for (let i = 0; i < length; ++i) {
-      const x = Math.floor(Math.random() * length)
-      const y = Math.floor(Math.random() * length)
-      const temp = this.data.objectArray[x]
-      this.data.objectArray[x] = this.data.objectArray[y]
-      this.data.objectArray[y] = temp
-    }
-    this.setData({
-      objectArray: this.data.objectArray
-    })
-  },
-  addToFront(e) {
-    const length = this.data.objectArray.length
-    this.data.objectArray = [{ id: length, unique: 'unique_' + length }].concat(this.data.objectArray)
-    this.setData({
-      objectArray: this.data.objectArray
-    })
-  },
-  addNumberToFront(e) {
-    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
-    this.setData({
-      numberArray: this.data.numberArray
-    })
-  },
   /**
    * 生命周期函数--监听页面加载
    */
+
+  /**
+     * 点击导航栏
+      */
+     onNavBarTap(event){
+         // 获取点击的navbar的index
+       console.log(event)
+         let navbarTapIndex = event.currentTarget.dataset.navbarIndex
+       console.log(navbarTapIndex)
+         // 设置data属性中的navbarActiveIndex为当前点击的navbar
+         this.setData({
+             navbarActiveIndex: navbarTapIndex      
+
+    })
+  },
+
+  onBindAnimationFinish: function ({ detail }) {
+         // 设置data属性中的navbarActiveIndex为当前点击的navbar
+         this.setData({
+             navbarActiveIndex: detail.current
+     })
+    
+  },
+
   onLoad: function (options) {
 
     
